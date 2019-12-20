e5d7a30f0fd527641e7cb1e419e5449f7d474989
kbradwell <kbradwell@users.noreply.github.com>
2019-12-19 22:32:16 -0500

    Add files via upload

---
+++ read_counts/run_normalize_count.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/local/packages/Python-2.7/bin/python apply_libsize_scalingfactor.py str_pysamcounts_hg3_noglobin.out str_pysamcounts_hg3_noglobin.normalized.out
+
+/usr/local/packages/Python-2.7/bin/python apply_libsize_scalingfactor.py str_pysamcounts_hg3_noglobin_autosomes.out str_pysamcounts_hg3_noglobin_autosomes.normalized.out
